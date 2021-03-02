8ea315a64009ca64df3db521a4fd9a8e7cf4d45f
camille <spacejamilli20@gmail.com>
2021-03-02 10:49:38 -0600

    fixed form div

--- front-end/src/Components/Login.js
+++ front-end/src/Components/Login.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import {useState} from 'react'
 
 
@@ -42,7 +42,7 @@ function Login() {
 
                 </div>
             </form>
-        </>
+        </div>
     )
 
 }
