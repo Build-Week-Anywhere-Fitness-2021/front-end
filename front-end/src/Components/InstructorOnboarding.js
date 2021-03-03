0753920c8fbddeeeda434808761470ccabf05652
Bri <brittanymorris@MacBook-Pro.local>
2021-03-02 19:14:33 -0700

    Added Lorem Ipsom to the page.

--- front-end/src/Components/InstructorOnboarding.js
+++ front-end/src/Components/InstructorOnboarding.js
@@ -9,7 +9,15 @@ function PageOne(){
     }
     return(
         <div>
-            <h1>Page 1</h1>
+            <h1 className = 'onboarding-title'>Page 1</h1>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse magna augue, faucibus ut viverra sit amet, scelerisque eu velit. 
+            Nullam sed est sed nibh pretium convallis. Proin accumsan odio in nisi lobortis, sed lacinia felis luctus. Nullam maximus lacus a efficitur facilisis. 
+            Sed volutpat vestibulum quam, sit amet pellentesque est pharetra id. Duis in tortor sit amet lectus euismod interdum a vitae augue. Mauris sodales 
+            ac magna eu commodo. Sed posuere ultricies est. Nullam vehicula faucibus magna, a sodales massa posuere et. Mauris nulla felis, finibus nec egestas
+            non, congue at ex. Morbi eu leo orci. Mauris non ligula vel metus efficitur maximus. Quisque nec nibh id sem cursus gravida. Donec nec est ut orci 
+            ringilla ultricies. In ultrices suscipit orci a lobortis
+            </p>
             <div onClick = {routeToPage2} className="oboarding-button">Next</div>
         </div>
     )
